46d2c6a990822e42ccf4d17cf7dba3658d2a0c18
Yaikwhed <yaikwhed@aim.com>
2016-09-29 22:55:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,14 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexKya = /(.|)*kya!~/;
   
-  var waifuPhrases = [ "The best is certainly _"]
+  var waifuPhrases = [ "It's not like I l-like you or anything...", "_-kun is so moe!", "Do you think I'm kawaii, _?",
+                      "B-B-baka!", "_-senpai is the best!", "But isn't that... lewd?", "Kemy-kun is sugoi, but not as sugoi as _-senpai!", "Noooo!",
+                     "Your waifu is trashfu!", "http://i.imgur.com/8JIV2U5.png", "https://pbs.twimg.com/media/CmIzPnkUoAQAG0f.png",
+                     "I could never hate you, _-kun!", "Do you really mean it, _-senpai?", "Rolls? Why do they call her Rolls?", 
+                     "He's not really a mummy, is he?", "Happy waifu, happy laifu!", 
+                     "https://lh3.googleusercontent.com/-SYHqWZDW36s/VX7rAdDfzjI/AAAAAAAAC6E/QWhg1-0IEhY/w1280-h720/hyperdimension_neptunia-01-neptune-heroine-peace_sign-pose-hair_ornaments.jpg",
+                     "I m-made you tendies _-kun. I hope you like them...", "To me, you have all the goodboy points in the world.",
+                     "Hitler-sama did nothing wrong."]
 
   if(request.text && botRegexKya.test(request.text)) {
     this.res.writeHead(200);
