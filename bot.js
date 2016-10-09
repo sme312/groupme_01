84b94ea467de00eeb18e0c15fdcb6ef96d09e759
sme312 <smevans312@gmail.com>
2016-10-09 13:38:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,12 +17,15 @@ var hp_enemy = 50;
 var accuracy_check = 0;
 var combat_check = false;
 var go_into_combat = true;
+var combat = false;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexKya = /(.|)*kya!~/;
       botname = /(.|)*kyaa!~/;
       botsave = /\bsave\b/;
+      botcombat = /^cambat^/
+  
   
   var waifuPhrases = [ "https://pbs.twimg.com/media/B8YdqjxIQAAU87L.jpg", "It's not like I l-like you or anything...", 
                       "B-B-baka!", "My senpai is the best!", "But isn't that... lewd?", "Kemy-kun is sugoi, but not as sugoi as senpai!", "Noooo!",
@@ -41,66 +44,74 @@ function respond() {
     saveProgress();
     this.res.end();
   }
-else if (combat_check = false && go_into_combat = true) {
-combat();	
-}
-else if (combat_check = true && hp_yours > 0 && hp_enemy > 0) {
-combat();
-}
-else if (combat_check = true && hp_yours > 0 && hp_enemy <= 0) {
-window.alert("You won GG EZ \n");
-combat_check = false;
-go_into_combat = false;
-}
-else if (combat_check = true && hp_yours <= 0 && hp_enemy > 0) {
-window.alert("Your opponent won GG NO REEEEEEEEEEEEEEEEEEEEEE \n");
-combat_check = false;
-go_into_combat = false;
-}
-else if (combat_check = true && hp_yours <=0 && hp_enemy <= 0) {
-window.alert("You both killed each other RIPIPIP \n");
-combat_check = false;
-go_into_combat = false;
-
-}
-else if (combat_check = false && go_into_combat = false) {
-window.alert("Your are not in combat \n");
-}	
-  else {
-    console.log("Nothing happened");
-    this.res.writeHead(200);
+   else if(request.text && botcombat.test(request.text)) {
+    combat = true;
     this.res.end();
   }
+
+while(combat){
+if (combat_check === false && go_into_combat === true) {
+combatYou();
+combatEnemy();
+}
+else if (combat_check === true && hp_yours > 0 && hp_enemy > 0) {
+combatYou();
+combatEnemy();
+}
+else if (combat_check === true && hp_yours > 0 && hp_enemy <= 0) {
+postMessage("You won GG EZ \n");
+combat_check = false;
+go_into_combat = false;
+combat = false;
+}
+else if (combat_check === true && hp_yours <= 0 && hp_enemy > 0) {
+postMessage("Your opponent won GG NO REEEEEEEEEEEEEEEEEEEEEE \n");
+combat_check = false;
+go_into_combat = false;
+combat = false;
+}
+else if (combat_check === true && hp_yours <=0 && hp_enemy <= 0) {
+postMessage("You both killed each other RIPIPIP \n");
+combat_check = false;
+go_into_combat = false;
+combat = false;
+
+}
+else if (combat_check === false && go_into_combat === false) {
+postMessage("Your are not in combat \n");
 }
 
+  
 function combatYou(){
 combat_check = true;
 accuracy_check = (Math.floor(Math.random()* (100 - 0)) + 0);
 if (accuracy_check <= accuracy_yours) {
 hp_enemy = hp_enemy - (attack_yours*defense_enemy*.01);
-hp_enemy_string = Integer.toString(hp_enemy)
+//hp_enemy_string = Integer.toString(hp_enemy);
 //window.alert("Your attack hit, your enemy's health is " + hp_enemy_string +"\n");
-return hp_enemy_string;
+postMessage("Your attack hit, your enemy's health is " + hp_enemy +"\n");
 }
 else if (accuracy_check > accuracy_yours) {
 //window.alert("Your attack did not hit \n");
-return kys = "KYS1";
+postMessage("Your attack did not hit \n");
 }
 }
-
-function combatEnemy(){
+  
+ function combatEnemy(){
 accuracy_check = (Math.floor(Math.random()* (100 - 0)) + 0);
 if (accuracy_check <= accuracy_enemy) {
 hp_yours = hp_yours - (attack_enemy*defense_yours*.01);
-hp_yours_string = Integer.toString(hp_yours)
+//hp_yours_string = Integer.toString(hp_yours);
 //window.alert("Your enemy's attack hit, your health is " + hp_yours_string + "\n");
-return hp_yours_string;
+postMessage("Your enemy's attack hit, your health is " + hp_yours + "\n");
 }
 else if (accuracy_check > accuracy_enemy) {
 //window.alert("Your enemy's attack did not hit \n");
-return kys = "KYS2";
+postMessage("Your enemy's attack did not hit \n");
 }
 }
+}
+ 
 
 function postMessage(response) {
   var botResponse,options, body, botReq;
