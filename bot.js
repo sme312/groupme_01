633a2a1a23ddff9d1dc41b39706b73150ac489cc
sme312 <smevans312@gmail.com>
2016-10-08 20:18:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexKya = /(.|)*kya!~/;
       botname = /(.|)*kyaa!~/;
-      botsave = /(.|)*\bsave\b/;
+      botsave = /(.|)*\psave\p/;
   
   var waifuPhrases = [ "https://pbs.twimg.com/media/B8YdqjxIQAAU87L.jpg", "It's not like I l-like you or anything...", 
                       "B-B-baka!", "My senpai is the best!", "But isn't that... lewd?", "Kemy-kun is sugoi, but not as sugoi as senpai!", "Noooo!",
@@ -36,7 +36,7 @@ function respond() {
 }
 
 function saveProgress() {
-  //int number = Math.random();
+  
   postMessage("saved");
 }
 
