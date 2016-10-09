58e42c5d990802d2b1608dd0edcb1342fd32dee8
sme312 <smevans312@gmail.com>
2016-10-09 14:25:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -181,13 +181,10 @@ function respond() {
       botsavecode = /(.|)*\breenter\b/;
 
   //create new character
- var waifuPhrases = [ "https://pbs.twimg.com/media/B8YdqjxIQAAU87L.jpg", "It's not like I l-like you or anything...", 
-"B-B-baka!", "My senpai is the best!", "But isn't that... lewd?", "Kemy-kun is sugoi, but not as sugoi as senpai!", "Noooo!",
-"http://i0.kym-cdn.com/photos/images/facebook/000/240/558/d76.jpg", "http://2.bp.blogspot.com/-6hX2FngcmZk/U1VlHs5CfNI/AAAAAAAAQNI/yxSWLiV-z94/s1600/waifu.png"]
-
+ 
 if(request.text && botRegexKya.test(request.text)) {
 this.res.writeHead(200);
-postMessage(waifuPhrases[getRandomInt(0,waifuPhrases.length)]);
+postMessage("please");
 this.res.end();
 }
 else if(request.text && botsave.test(request.text)) {
@@ -290,40 +287,6 @@ this.res.end();
   }
 }
 
-function postMessage(response) {
-  var botResponse,options, body, botReq;
-
-  botResponse = response
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -407,6 +370,7 @@ postMessage("Your saved game code is: " + charSaveCode.toString());
 
 }
 
+
 function decode(saveCode){
 var digitArray = [];
 for(i = 0; i < 15; i++){
@@ -424,7 +388,7 @@ checkpoint = digitArray[14];
 postMessage("Your attack is: "  + evasiveness);
 
 }
-
+ 
 function postMessage(response) {
 var botResponse,options, body, botReq;
 
@@ -440,6 +404,7 @@ body = {
 "bot_id" : botID,
 "text" : botResponse
 };
+
 
 console.log('sending ' + botResponse + ' to ' + botID);
 
